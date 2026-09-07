9cebe920dd8e4dfc08fd796d87471c785ecec9e7
Allan Pierre <allanpie2405@gmail.com>
2026-09-07 18:01:36 -0300

    implementing task loading on TaskViewer(i forget to add new files to commit)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-function App(){
-    return(
-        <div className="w-125">
-            
+import { useState } from "react"
+import TaskViewer from "./components/TaskViewer"
+
+function App() {
+    const [tasks, setTasks] = useState([
+        { id: 1, title: "Task 1", completed: false },
+        { id: 2, title: "Task 2", completed: true },
+        { id: 3, title: "Task 3", completed: false },
+        { id: 4, title: "Task 4", completed: false },
+    ])
+
+    return (
+        <div className="w-screen h-screen flex justify-center">
+            <div className="w-[500px]">
+                <TaskViewer tasks={tasks} />
+            </div>
         </div>
     )
 }
--- src/components/TaskViewer.jsx
+++ src/components/TaskViewer.jsx
@@ -1,7 +1,15 @@
-function TaskViewer(){
-    return(
-        <div>
-            
+import { Library, Trash2Icon } from "lucide-react"
+
+function TaskViewer(props) {
+    return (
+        <div className="bg-vanillaCream p-4 rounded-md">
+            <ul className="space-y-1">
+                {props.tasks.map((task) => (
+                    <li className="bg-midnightIndigo text-vanillaCream rounded-md p-1"
+                        key={task.id}>{task.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
